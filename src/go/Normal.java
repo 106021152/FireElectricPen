989e532f333ee4c7c8f13c0ef21a412e0a79f76d
106021152 <32092982+106021152@users.noreply.github.com>
2018-12-19 16:03:00 +0800

    Add files via upload

--- src/go/Normal.java
+++ src/go/Normal.java
@@ -8,33 +8,65 @@ public class Normal extends JFrame {
     private JMenuBar jmb = new JMenuBar();
     private JMenu jmusystem = new JMenu("System");
     private JMenu jmuabout = new JMenu("About");
-    private JButton jbtnpause = new JButton("");
     private JMenuItem jmiback = new JMenuItem("Back");
     private JMenuItem jmiexit = new JMenuItem("Exit");
     private JMenuItem jmiabout = new JMenuItem("關於作者");
     private Final frame;
-    private JLabel stick = new JLabel();
-    private ImageIcon img1 = new ImageIcon("stick.PNG");
+    private JButton jbtnpause = new JButton("");
     private ImageIcon img4 = new ImageIcon("pause.png");
     private ImageIcon img5 = new ImageIcon("start2.png");
-    private Finalgo framenormal;
+    private Sprite stick = new Sprite();
+    private Sprite boom  = new Sprite();
+    private Sprite wall1 = new Sprite();
+    private Sprite wall2 = new Sprite();
+    private Sprite wall3 = new Sprite();
+    private Sprite wall4 = new Sprite();
+    private Sprite wall5 = new Sprite();
+    private Sprite wall6 = new Sprite();
+    private Sprite wall7 = new Sprite();
+    private Sprite wall8 = new Sprite();
+    private Sprite wall9 = new Sprite();
+    private Sprite wall10 = new Sprite();
+    private Sprite wall11 = new Sprite();
+    private Sprite wall12 = new Sprite();
+    private Sprite wall13 = new Sprite();
+    private Sprite wall14 = new Sprite();
+    private Sprite wall15 = new Sprite();
+    private Sprite wall16 = new Sprite();
+    private Sprite wall17 = new Sprite();
+    private Sprite wall18 = new Sprite();
+    private Sprite wall19 = new Sprite();
+    private Sprite wall20 = new Sprite();
+    private Sprite wall21 = new Sprite();
+    private Sprite wall22 = new Sprite();
+    private Sprite wall23 = new Sprite();
+    private Sprite wall24 = new Sprite();
+    private Sprite wall25 = new Sprite();
+    private Sprite wall26 = new Sprite();
+    private Sprite finish = new Sprite();
+    private ImageIcon img1 = new ImageIcon("stick.PNG");
+    private ImageIcon img2 = new ImageIcon("boom.png");
     private Container cp;
     private Timer bgTimer;
     private int bg = 1;
-
-    public Normal(Finalgo framenormal){
-        this.framenormal = framenormal;
+    private Finalgo frameeasy;
+    public Normal(Finalgo frameeasy){
+        this.frameeasy = frameeasy;
         init();
     }
 
     public void init(){
-        this.setJMenuBar(jmb);
+        this.setLayout(null);
         jmb.add(jmusystem);
         jmb.add(jmuabout);
         jmusystem.add(jmiback);
         jmusystem.add(jmiexit);
         jmuabout.add(jmiabout);
-        cp =this.getContentPane();
+        cp=this.getContentPane();
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setBounds(0,0,1600,1000);
+        this.setResizable(false);
+        this.setTitle("電流急急棒");
         jbtnpause.setBounds(1500,10,80,80);
         jbtnpause.setFont(new Font(null,Font.BOLD,30));
         jbtnpause.setContentAreaFilled(false);
@@ -42,18 +74,153 @@ public class Normal extends JFrame {
         jbtnpause.setForeground(Color.RED);
         this.add(jbtnpause);
         jbtnpause.setIcon(img4);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setBounds(0,0,1600,1000);
-        this.setResizable(false);
-        this.setTitle("電流急急棒");
+        stick.setPosition(0, 500, 80, 48);
+        stick.customRange(40, 14, 5, 16);    //****
         stick.setIcon(img1);
+
+        boom.setPosition(0,500,80,48);
+        stick.customRange(40,14,5,16);
+        wall1.setPosition(0, 480, 110, 15);
+        wall1.setBackground(Color.BLACK);
+        wall1.setOpaque(true);
+
+        wall2.setPosition(0, 550, 190, 15);
+        wall2.setBackground(Color.BLACK);
+        wall2.setOpaque(true);
+
+        wall3.setPosition(95, 285, 15, 200);
+        wall3.setBackground(Color.BLACK);
+        wall3.setOpaque(true);
+
+        wall4.setPosition(175, 355, 15, 200);
+        wall4.setBackground(Color.BLACK);
+        wall4.setOpaque(true);
+
+        wall5.setPosition(105, 285, 300, 15);
+        wall5.setBackground(Color.BLACK);
+        wall5.setOpaque(true);
+
+        wall6.setPosition(175, 345, 165, 15);
+        wall6.setBackground(Color.BLACK);
+        wall6.setOpaque(true);
+
+        wall7.setPosition(325, 355, 15, 330);
+        wall7.setBackground(Color.BLACK);
+        wall7.setOpaque(true);
+
+        wall8.setPosition(405, 285, 15, 340);
+        wall8.setBackground(Color.BLACK);
+        wall8.setOpaque(true);
+
+        wall9.setPosition(325, 680, 350, 15);
+        wall9.setBackground(Color.BLACK);
+        wall9.setOpaque(true);
+
+        wall10.setPosition(405, 620, 180, 15);
+        wall10.setBackground(Color.BLACK);
+        wall10.setOpaque(true);
+
+        wall11.setPosition(675, 405, 15, 290);
+        wall11.setBackground(Color.BLACK);
+        wall11.setOpaque(true);
+
+        wall12.setPosition(575, 335, 15, 300);
+        wall12.setBackground(Color.BLACK);
+        wall12.setOpaque(true);
+
+        wall13.setPosition(675, 405, 200, 15);
+        wall13.setBackground(Color.BLACK);
+        wall13.setOpaque(true);
+
+        wall14.setPosition(575, 335, 200, 15);
+        wall14.setBackground(Color.BLACK);
+        wall14.setOpaque(true);
+
+        wall15.setPosition(770, 150, 15, 200);
+        wall15.setBackground(Color.BLACK);
+        wall15.setOpaque(true);
+
+        wall16.setPosition(875, 220, 15, 200);
+        wall16.setBackground(Color.BLACK);
+        wall16.setOpaque(true);
+
+        wall17.setPosition(875, 220, 100, 15);
+        wall17.setBackground(Color.BLACK);
+        wall17.setOpaque(true);
+
+        wall18.setPosition(775, 150, 300, 15);
+        wall18.setBackground(Color.BLACK);
+        wall18.setOpaque(true);
+
+        wall19.setPosition(975, 220, 15, 250);
+        wall19.setBackground(Color.BLACK);
+        wall19.setOpaque(true);
+
+        wall20.setPosition(1075, 150, 15, 250);
+        wall20.setBackground(Color.BLACK);
+        wall20.setOpaque(true);
+
+        wall21.setPosition(1075, 400, 150, 15);
+        wall21.setBackground(Color.BLACK);
+        wall21.setOpaque(true);
+
+        wall22.setPosition(975, 460, 150, 15);
+        wall22.setBackground(Color.BLACK);
+        wall22.setOpaque(true);
+
+        wall23.setPosition(1225, 400, 15, 200);
+        wall23.setBackground(Color.BLACK);
+        wall23.setOpaque(true);
+
+        wall24.setPosition(1125, 460, 15, 250);
+        wall24.setBackground(Color.BLACK);
+        wall24.setOpaque(true);
+
+        wall25.setPosition(1125, 700, 480, 15);
+        wall25.setBackground(Color.BLACK);
+        wall25.setOpaque(true);
+
+        wall26.setPosition(1225, 600, 450, 15);
+        wall26.setBackground(Color.BLACK);
+        wall26.setOpaque(true);
+        finish.setPosition(1550,610,40,100);
+        finish.setBackground(Color.RED);
+        finish.setOpaque(true);
+
         this.add(stick);
+        this.add(wall1);
+        this.add(wall2);
+        this.add(wall3);
+        this.add(wall4);
+        this.add(wall5);
+        this.add(wall6);
+        this.add(wall7);
+        this.add(wall8);
+        this.add(wall9);
+        this.add(wall10);
+        this.add(wall11);
+        this.add(wall12);
+        this.add(wall13);
+        this.add(wall14);
+        this.add(wall15);
+        this.add(wall16);
+        this.add(wall17);
+        this.add(wall18);
+        this.add(wall19);
+        this.add(wall20);
+        this.add(wall21);
+        this.add(wall22);
+        this.add(wall23);
+        this.add(wall24);
+        this.add(wall25);
+        this.add(wall26);
+        this.add(finish);
         bgTimer = new Timer(200, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (bg == 1){
 //                    jlb.setIcon(imageIcon[0]);
-                   cp.setBackground(Color.gray);
+                    cp.setBackground(Color.gray);
                     bg = 2;
                 }else if (bg == 2){
 //                    jlb.setIcon(imageIcon[1]);
@@ -63,24 +230,62 @@ public class Normal extends JFrame {
             }
         });
         bgTimer.start(); //在timer以下
+
         stick.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                stick.setLocation(e.getXOnScreen() -50,e.getYOnScreen()-510 );
+
+                stick.changePosition(stick.getX() + e.getX() - stick.getWidth()/2, stick.getY() + e.getY() - stick.getHeight()/2);
+
             }
         });
-
+        jbtnpause.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton tmpbtn = (JButton) e.getSource();
+                if (tmpbtn.getIcon().equals(img5)) {          //tmpbtn.getText().equals("turn on")
+                    bgTimer.start();
+                    tmpbtn.setIcon(img4);
+                } else {
+                    bgTimer.stop();
+                    tmpbtn.setIcon(img5);
+                }
+            }
+        });
         stick.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                stick.setLocation(e.getXOnScreen() -50 ,e.getYOnScreen() - 510);
+//                System.out.println("x1: " + stick.xStart);
+//                System.out.println("x2: " + stick.xEnd);
+//                System.out.println("y1: " + stick.yStart);
+//                System.out.println("y2: " + stick.yEnd);
+
+                stick.changePosition(stick.getX() + e.getX() - stick.getWidth()/2, stick.getY() + e.getY() - stick.getHeight()/2);
+
+                if (!e.isControlDown()) {           //外掛
+
+                    if (stick.overlapCheck(wall1) || stick.overlapCheck(wall2) || stick.overlapCheck(wall3) || stick.overlapCheck(wall4) || stick.overlapCheck(wall5) || stick.overlapCheck(wall6) || stick.overlapCheck(wall7) || stick.overlapCheck(wall8) || stick.overlapCheck(wall9) || stick.overlapCheck(wall10)) {
+                        stick.setIcon(img2);
+                        JOptionPane.showMessageDialog(Normal.this, "豬喔碰到了啦");
+                        stick.setIcon(img1);
+                        stick.setPosition(0, 500, 80, 48);
+                    } else if (stick.overlapCheck(finish)) {
+                        JOptionPane.showMessageDialog(Normal.this, "過關唷");
+                        Normal.this.setVisible(false);
+
+                    }
+
+                }
             }
         });
+
+
         jmiabout.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null,"子堯&健伊製作");
             }
         });
+
     }
 }
